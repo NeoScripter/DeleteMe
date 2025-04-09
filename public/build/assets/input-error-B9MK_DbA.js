221169e1e61eb9c7091fa6aa8ebed66ce8ad770c
Ilya <sange0337@gmail.com>
2025-04-09 18:08:48 +0800

    change plan prices

---
+++ public/build/assets/input-error-B9MK_DbA.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-AY2jYFEs.js";import{a as o}from"./createLucideIcon-dY24wDXB.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
